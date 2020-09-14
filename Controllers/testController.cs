c735fa9baa3453ae3b42a26ea33edd94ba6fe1b4
xutongyuan <123291779@qq.com>
2020-09-14 21:20:50 +0800

    initial commit

---
+++ Controllers/testController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class testController : Controller
+    {
+        public void FirstAction() 
+        {
+            string a = Request.Query["name"];
+           
+            string b = Request.Form["name"];
+            string b = Request.Form("name");
+            string c = Request.Headers["name"];
+            string d = Request.Cookies["name"];
+            HttpContext.Session.SetString("name","test");
+            HttpContext.Session.GetString("name");
+
+            TestModel entity = new TestModel();
+            entity.Name = "abc";
+            string Name = entity.Name;
+
+
+        }
+    }
+}
---
+++ Models/TestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class TestModel
+    {
+        int id;
+        string name;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+
+
+    }
+}
